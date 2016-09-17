9cae87aa7d4e59cbbb652f477926b3f51c14a3de
idk <idk@devuan.local>
2016-09-17 18:26:44 -0400

    is it a naming thing?

--- helper.js
+++ helper.js
@@ -9,27 +9,27 @@ function genTorrent(form){
                                 blob = xhr.response;//xhr.response is now a blob object
                                 blob.name=file;
                                 // Validate name and turn into string
-                                var name = (form.fileName.value == "none" ) ? null : form.fileName.value;
+                                var b_name = (form.fileName.value == "none" ) ? null : form.fileName.value;
                                 blob.name = (name != null) ? blob.name : name;
-                                var creator = (form.createdBy.value == "none") ? null : form.createdBy.value;
+                                var b_creator = (form.createdBy.value == "none") ? null : form.createdBy.value;
                                 // Validate comment and turn into string
-                                var comment = (form.comment.value == "none") ? null : form.comment.value;
+                                var b_comment = (form.comment.value == "none") ? null : form.comment.value;
                                 // Validate time and turn into string
-                                var metadate = (form.creationDate.value == "none") ? null : form.creationDate.value;
+                                var b_metadate = (form.creationDate.value == "none") ? null : form.creationDate.value;
                                 // Determine if the user wants the torrent to be private and turn into bool
-                                var privacy = (form.privateTorrent.value == "true") ? true : false;
+                                var b_privacy = (form.privateTorrent.value == "true") ? true : false;
                                 // Set an arbitrary piece length
-                                var piecelen = (form.pieceLength.value == "none") ? null: form.pieceLength.value;
+                                var b_piecelen = (form.pieceLength.value == "none") ? null: form.pieceLength.value;
                                 // Turn supplemental announcers into array of strings, comma-separated
-                                var announcers = (form.announceList.value == "none") ? null : form.announceList.value.split(",");
+                                var b_announcers = (form.announceList.value == "none") ? null : form.announceList.value.split(",");
                                 // Turn supplemental webseeds into array of strings, comma-separated
-                                var webseeds = (form.urlList.value == "none") ? null : form.urlList.value.split(",");
+                                var b_webseeds = (form.urlList.value == "none") ? null : form.urlList.value.split(",");
                                 /*options = { name: blob.name, comment: comment, createdBy: creator, creationDate: metadate, private: privacy, pieceLength: piecelen, announceList: announcers, urlList: webseeds};*/
-                                createTorrent(blob, { name: 'My Cool Torrent' } , function (err, torrent) {
+                                createTorrent(blob, { name: b_name } , function (err, torrent) {
                                         if ( !err ) {
-                                                alert( "SUCCESS", "; ", file, "; " , name, "; " , creator, "; " , comment, "; " , metadate, "; " , privacy, "; " , piecelen, "; " , announcers, "; " , webseeds );
+                                                alert( "SUCCESS", "; ", file, "; " , b_name, "; " , b_creator, "; " , b_comment, "; " , b_metadate, "; " , b_privacy, "; " , b_piecelen, "; " , b_announcers, "; " , b_webseeds );
                                         }else{
-                                                alert( "ERROR", "; ", file, "; " , name, "; " , creator, "; " , comment, "; " , metadate, "; " , privacy, "; " , piecelen, "; " , announcers, "; " , webseeds );
+                                                alert( "ERROR", "; ", file, "; " , b_name, "; " , b_creator, "; " , b_comment, "; " , b_metadate, "; " , b_privacy, "; " , b_piecelen, "; " , b_announcers, "; " , b_webseeds );
                                         }
                                 })
                         }
